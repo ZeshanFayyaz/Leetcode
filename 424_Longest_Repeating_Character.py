13c5504151cfc3e69033266f0155a34108f92a1d
Zeshan <zeshanf59@hotmail.com>
2025-05-07 18:29:32 -0400

    Added soln

---
+++ 424_Longest_Repeating_Character.py
@@ -0,0 +1,20 @@
+class Solution:
+    def characterReplacement(self, s: str, k: int) -> int:
+        count = {}
+        length = 0
+
+        left = 0
+        right = 0
+
+        for right in range(len(s)):
+            temp = s[right]
+            count[temp] = 1 + count.get(temp, 0)
+
+            if (right - left + 1) - max(count.values()) > k:
+                count[s[left]] -= 1
+                left += 1
+
+            length = max(length, right - left + 1)
+        return length
+
+
